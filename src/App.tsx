9d5c7eaf610f4ce30599e4c7edd9df9a155bbfa4
Mateja Petrovic <mateja176@gmail.com>
2019-05-19 19:40:16 +0200

    creates html editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,49 @@
 import { Button, TextField } from '@material-ui/core';
-import React, { SyntheticEvent, useState } from 'react';
+import React, { SyntheticEvent, useRef, useState } from 'react';
 
 const App: React.FC = () => {
   const [html, setHtml] = useState('');
 
   const handleSubmit = <E extends SyntheticEvent>(e: E) => {
     e.preventDefault();
-
-    console.log(html);
   };
 
-  return (
-    <form
-      onSubmit={handleSubmit}
-      style={{
-        display: 'grid',
-        gridGap: 10,
-      }}
-    >
-      <TextField
-        value={html}
-        placeholder="Hello World"
-        multiline={true}
-        rows={20}
-        fullWidth
-        onChange={({ target: { value } }) => {
-          setHtml(value);
-        }}
-        onKeyPress={e => {
-          const { key, ctrlKey } = e;
+  const contentRef = useRef<HTMLDivElement>();
 
-          if (key === 'Enter' && ctrlKey) {
-            handleSubmit(e);
-          }
+  console.log(contentRef);
+
+  return (
+    <main>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: 'grid',
+          gridGap: 10,
         }}
-      />
-      <Button variant="contained" type="submit">
-        Submit
-      </Button>
-    </form>
+      >
+        <TextField
+          value={html}
+          placeholder="Hello World"
+          multiline={true}
+          rows={20}
+          fullWidth
+          onChange={({ target: { value } }) => {
+            setHtml(value);
+          }}
+          onKeyPress={e => {
+            const { key, ctrlKey } = e;
+
+            if (key === 'Enter' && ctrlKey) {
+              handleSubmit(e);
+            }
+          }}
+        />
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+      </form>
+      <div ref={contentRef as any} dangerouslySetInnerHTML={{ __html: html }} />
+    </main>
   );
 };
 
