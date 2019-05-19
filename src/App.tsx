b9ecf0c9bbcbce5facd0ff2c0529e85187300168
Mateja Petrovic <mateja176@gmail.com>
2019-05-19 18:54:43 +0200

    adds form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, TextField } from '@material-ui/core';
+import React, { SyntheticEvent, useState } from 'react';
 
 const App: React.FC = () => {
+  const [html, setHtml] = useState('');
+
+  const handleSubmit = <E extends SyntheticEvent>(e: E) => {
+    e.preventDefault();
+
+    console.log(html);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: 'grid',
+        gridGap: 10,
+      }}
+    >
+      <TextField
+        value={html}
+        placeholder="Hello World"
+        multiline={true}
+        rows={20}
+        fullWidth
+        onChange={({ target: { value } }) => {
+          setHtml(value);
+        }}
+        onKeyPress={e => {
+          const { key, ctrlKey } = e;
+
+          if (key === 'Enter' && ctrlKey) {
+            handleSubmit(e);
+          }
+        }}
+      />
+      <Button variant="contained" type="submit">
+        Submit
+      </Button>
+    </form>
   );
-}
+};
 
 export default App;
